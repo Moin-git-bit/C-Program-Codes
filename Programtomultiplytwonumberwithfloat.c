acb8ecccede32a782651dc2bba9929af21517e94
Moin-git-bit <92537244+Moin-git-bit@users.noreply.github.com>
2021-10-26 18:10:21 +0530

    Create Programtomultiplytwonumberwithfloat.c

---
+++ Programtomultiplytwonumberwithfloat.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+  int main()
+{
+      int a,b,product;
+    printf("\n\t\a Enter two number\n\t\a");
+      scanf("%d %d",&a, &b);
+
+    //calculating two numbers
+      product = a*b;
+    printf("\n\t\a product =%d",product);
+      getch();
+    return 0;
+
+
+}
